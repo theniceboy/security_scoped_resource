162b091ea28c5fad381cddc909117ed8f102fb91
rmri <intonarumori@gmail.com>
2023-06-09 16:00:12 +0200

    fixed missing use of isDirectory

--- ios/Classes/SwiftSecurityScopedResourcePlugin.swift
+++ ios/Classes/SwiftSecurityScopedResourcePlugin.swift
@@ -20,14 +20,14 @@ public class SwiftSecurityScopedResourcePlugin: NSObject, FlutterPlugin, UIDocum
                 result(FlutterError(code: "InvalidArguments", message: "argument 'path' is not a String or argument 'isDirectory' is not a Bool", details: nil))
                 return
             }
-            let url = URL(fileURLWithPath: dir, isDirectory: true)
+            let url = URL(fileURLWithPath: dir, isDirectory: isDirectory)
             result(url.startAccessingSecurityScopedResource())
         case "stopAccessingSecurityScopedResource":
             guard let dir = args["path"] as? String, let isDirectory = args["isDirectory"] as? Bool else {
                 result(FlutterError(code: "InvalidArguments", message: "argument 'path' is not a String or argument 'isDirectory' is not a Bool", details: nil))
                 return
             }
-            let url = URL(fileURLWithPath: dir, isDirectory: true)
+            let url = URL(fileURLWithPath: dir, isDirectory: isDirectory)
             url.stopAccessingSecurityScopedResource()
             result(true)
         default:
